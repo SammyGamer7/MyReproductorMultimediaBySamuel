e685c85af3aed93f312f7d19afc3ee4fd5b19531
Sammy <Sammy@DESKTOP-O1DURCQ>
2022-10-30 19:09:46 -0600

    Agregar archivos de proyecto.

---
+++ MyReproductorMultimedia/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MyReproductorMultimedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mpMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.LbListaArchivos = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mpMediaPlayer
+            // 
+            this.mpMediaPlayer.Enabled = true;
+            this.mpMediaPlayer.Location = new System.Drawing.Point(12, 18);
+            this.mpMediaPlayer.Name = "mpMediaPlayer";
+            this.mpMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mpMediaPlayer.OcxState")));
+            this.mpMediaPlayer.Size = new System.Drawing.Size(508, 420);
+            this.mpMediaPlayer.TabIndex = 0;
+            this.mpMediaPlayer.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // LbListaArchivos
+            // 
+            this.LbListaArchivos.FormattingEnabled = true;
+            this.LbListaArchivos.Location = new System.Drawing.Point(526, 18);
+            this.LbListaArchivos.Name = "LbListaArchivos";
+            this.LbListaArchivos.Size = new System.Drawing.Size(211, 381);
+            this.LbListaArchivos.TabIndex = 1;
+            this.LbListaArchivos.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(648, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LbListaArchivos);
+            this.Controls.Add(this.mpMediaPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mpMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mpMediaPlayer;
+        private System.Windows.Forms.ListBox LbListaArchivos;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MyReproductorMultimedia/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//CREADO POR SAMUEL LOPEZ
+
+namespace MyReproductorMultimedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MediaFile file = LbListaArchivos.SelectedItem as MediaFile;
+            if(file != null)
+            {
+                mpMediaPlayer.URL = file.Path;
+                mpMediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Multiselect = true, ValidateNames = true, 
+                    Filter = "WMV|*.wmv|WAV|*.wav|MP3|*.mp3|MP4|*.mp4|MKV|*.mkv"
+            })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<MediaFile> file = new List<MediaFile>();
+                    foreach(string FileName in ofd.FileNames)
+                    {
+                        FileInfo fi = new FileInfo(FileName);
+                        file.Add(new MediaFile()
+                        {
+                            FileName =
+                            Path.GetFileNameWithoutExtension(fi.FullName),
+                            Path = fi.FullName
+                        });
+                    }
+                    LbListaArchivos.DataSource = file;
+                }
+            }   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LbListaArchivos.ValueMember = "Path";
+            LbListaArchivos.DisplayMember = "FileName";
+        }
+    }
+}
